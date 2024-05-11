b90fab66b46454a154984fbfc5f941d625d4b44c
Arlind03 <harasaniarlind@yahoo.com>
2024-05-11 12:28:50 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import parkingdata from './components/parkingdata';
 
+
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
   const [sidebarData, setSidebarData] = useState(null);
   const [parkings, setParkings] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [counts, setCounts] = useState({ true_count: 0, false_count: 0 });
 
  const endpoint = () => {
     fetch('http://localhost:8000/api/parkings/')  
@@ -30,13 +32,26 @@ function App() {
       });
   };  
 
-
-  
-
-    const handleMapClick = (data) => {
-      setSidebarData(data);
-      setShowSidebar(!showSidebar);  
+  const endpoint1 = () => {
+    // Function to fetch data from Django
+    const fetchData = async () => {
+      try {
+        // Replace 'http://localhost:8000/process_booleans/' with your actual Django server URL
+        const response = await axios.post('http://localhost:8000/process_booleans/', {
+          item1: true,
+          item2: false,
+          item3: true,
+          item4: false
+        });
+        setCounts(response.data); // Set the data received from Django to state
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        setIsLoading(false);
+      }
     };
+    fetchData();
+  };
 
   return (
     <div className="App">
@@ -55,14 +70,5 @@ function App() {
   </div>
 );
 }
-
-{/* <div>
-<nav className='navbar'>
-  <img src="src\assets\tirana.png" alt="Img" />
-  <div>Tirana Ime 2.0</div>
-  <img src="src\assets\vod.png"/>
-</nav>
-<Map data={data} setSidebarData={setSidebarData} setShowSidebar={setShowSidebar}/>
-{showSidebar && <Sidebar data={sidebarData}/>} */}
 
 export default App
